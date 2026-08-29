3536fa80a5557b3b80024cc83da78fb0541990ae
TheAutoScaler <311592308+TheAutoScaler@users.noreply.github.com>
2026-08-29 17:17:07 +0100

    chore: auto-sync config 2026-08-29T16:17:07Z

--- dotfiles/.bashrc.d/hooks.sh
+++ dotfiles/.bashrc.d/hooks.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 
 if command -v starship &>/dev/null; then
-	if [[ -z "${STARSHIP_AWS_CACHE_DIR:-}" ]]; then
-		STARSHIP_AWS_CACHE_DIR="$(
-			mktemp -d "${TMPDIR:-/tmp}/starship-aws.XXXXXX"
-		)"
-		export STARSHIP_AWS_CACHE_DIR
-		trap 'rm -rf "$STARSHIP_AWS_CACHE_DIR"' EXIT
-	fi
-
-	set_win_title() {
-		local title
-
-		if [[ "$PWD" == "$HOME" ]]; then
-			title="~"
-		else
-			title="$(basename "$PWD")"
-		fi
-
-		printf '\033]0; %s \007' "$title"
-	}
-
-	starship_precmd_user_func="set_win_title"
 	eval "$(starship init bash)"
+	function set_win_title() {
+		if [[ "$PWD" == "$HOME" ]]; then
+			local title="~"
+		else
+			local title=$(basename "$PWD")
+		fi
+		echo -ne "\033]0; $title \007"
+	}
+	starship_precmd_user_func="set_win_title"
 fi
 
 if command -v direnv &>/dev/null; then
